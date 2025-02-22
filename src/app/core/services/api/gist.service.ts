654f03e465d44a469214089a607c7c55b39dc5ad
Abdullah Bukhari <abdullah.bukhari@emumba.com>
2025-02-23 04:26:46 +0500

    service shift to core

--- src/app/core/services/api/gist.service.ts
+++ src/app/core/services/api/gist.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GistService {
+export class GistsService {
+  private readonly API_URL = 'https://api.github.com/gists';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPublicGists(page: number, perPage: number): Observable<any> {
+    return this.http.get(`${this.API_URL}/public?page=${page}&per_page=${perPage}`);
+  }
+
+  getGistById(id: string): Observable<any> {
+    return this.http.get(`${this.API_URL}/${id}`);
+  }
 }
