51ac8c221352a83c3c3a1e24666bf3f581526f3e
Abdullah Bukhari <abdullah.bukhari@emumba.com>
2025-02-23 04:27:56 +0500

    local service clean up

--- src/app/features/public-gists/services/gists.service.ts
+++ src/app/features/public-gists/services/gists.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class GistsService {
-  private readonly API_URL = 'https://api.github.com/gists/public';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class GistsService {
+//   private readonly API_URL = 'https://api.github.com/gists/public';
 
-  constructor(private http: HttpClient) {}
+//   constructor(private http: HttpClient) {}
 
-  getPublicGists(page: number, perPage: number): Observable<any> {
-    return this.http.get(`${this.API_URL}?page=${page}&per_page=${perPage}`);
-  }
-} 
+//   getPublicGists(page: number, perPage: number): Observable<any> {
+//     return this.http.get(`${this.API_URL}?page=${page}&per_page=${perPage}`);
+//   }
+// } 
