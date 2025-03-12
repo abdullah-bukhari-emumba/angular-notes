7afcb9c1b9f595603630fac55eacd8bee99c1395
Abdullah Bukhari <abdullah.bukhari@emumba.com>
2025-03-12 17:29:55 +0500

    ocktokit removal

--- src/app/core/services/github.service.ts
+++ src/app/core/services/github.service.ts
@@ -1,60 +1,29 @@
-import { inject, Injectable } from '@angular/core';
-import { Octokit } from 'octokit';
-import { AuthService } from './auth.service';
-import { from, Observable } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GithubService {
-  private octokit: Octokit | null = null;
-
   private https = inject(HttpClient);
 
-  constructor(private authService: AuthService, http: HttpClient) {
-    this.initOctokit();
-
-    // Subscribe to token changes
-    this.authService.githubToken$.subscribe((token) => {
-      this.initOctokit();
-    });
-  }
-
-  private initOctokit() {
-    const token = this.authService.getGithubToken();
-    if (token) {
-      this.octokit = new Octokit({
-        auth: token,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28',
-        },
-      });
-    } else {
-      this.octokit = null;
-    }
-  }
+  constructor(private authService: AuthService) {}
 
   createGist(
     description: string,
     files: { [key: string]: { content: string } },
     isPublic: boolean = true
   ): Observable<any> {
-    if (!this.octokit) {
-      throw new Error('Authentication required to create a gist');
-    }
-    let payload: any = {
+    const payload = {
       description,
       public: isPublic,
       files,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${localStorage.getItem('github_token')}`,
-      },
     };
     console.log('payload', payload);
 
-    return from(this.octokit.request('POST /gists', payload));
+    // The HttpInterceptor will automatically add the proper Authorization header.
+    return this.https.post('https://api.github.com/gists', payload);
   }
 }
